7c1f76f92bbcabf955dadf2a03038c8e1bb63bc6
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-27 11:21:02 +0300

    сдаёмся

--- app/src/main/java/com/example/mobile6lab/MainActivity.kt
+++ app/src/main/java/com/example/mobile6lab/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -40,8 +41,14 @@ fun Start (modifier: Modifier = Modifier) {
     var name by remember { mutableStateOf("") }
     var gender by remember { mutableStateOf("") }
     var country by remember { mutableStateOf("") }
-    var age by remember { mutableStateOf(18f) }
-    val countries = listOf("Россия", "Украина", "Беларусь", "Казахстан", "Другие")
+    var age by remember { mutableFloatStateOf(18f) }
+    val countries = listOf(
+        stringResource(R.string.Country_for_list_1),
+        stringResource(R.string.Country_for_list_2),
+        stringResource(R.string.Country_for_list_3),
+        stringResource(R.string.Country_for_list_4),
+        stringResource(R.string.Country_for_list_5)
+    )
     var expanded by remember { mutableStateOf(false) }
 
     val context = LocalContext.current
@@ -56,26 +63,88 @@ fun Start (modifier: Modifier = Modifier) {
         OutlinedTextField(
             value = name,
             onValueChange = { name = it },
-            label = { Text("Имя") },
+            label = { Text(stringResource(R.string.name_str_show)) },
             modifier = Modifier.fillMaxWidth()
         )
-    }
-}
+        Text(stringResource(R.string.gender_field))
+        Row {
+            RadioButton(
+                selected = gender == stringResource(R.string.male_gender),
+                onClick = { gender = context.getString(R.string.female_gender) }
+            )
+            Text(context.getString(R.string.male_gender), modifier = Modifier.padding(end = 16.dp))
+
+            RadioButton(
+                selected = gender == stringResource(R.string.male_gender),
+                onClick = { gender = context.getString(R.string.female_gender) }
+            )
+            Text(context.getString(R.string.female_gender))
+        }
+
+        Text("Страна: $country")
+        Box {
+            OutlinedTextField(
+                value = country,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(stringResource(R.string.choose_country_text_in_field)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                countries.forEach { countryOption ->
+                    DropdownMenuItem(
+                        text = { Text(countryOption) },
+                        onClick = {
+                            country = countryOption
+                            expanded = false
+                        }
+                    )
+                }
+            }
+            Button(
+                onClick = { expanded = true },
+                modifier = Modifier.align(Alignment.CenterEnd)
+            ) {
+                Text(stringResource(R.string.choose_on_butt_drop_menu_country))
+            }
+        }
+
+        Text(stringResource(R.string.age_in_str_to_us_slider, age.toInt().toString()))
+        Slider(
+            value = age,
+            onValueChange = { age = it },
+            valueRange = 0f..100f,
+            steps = 99, // Для целых чисел от 0 до 100
+            modifier = Modifier.fillMaxWidth()
+        )
 
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobile6LabTheme {
-        Greeting("Android")
+        Button(
+            onClick = {
+                if (name.isNotEmpty() && gender.isNotEmpty() && country.isNotEmpty()) {
+                    Toast.makeText(
+                        context,
+                        context.getString(
+                            R.string.data_save_to_user,
+                            name,
+                            gender,
+                            country,
+                            age.toInt().toString()
+                        ),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(context,
+                        context.getString(R.string.for_user_warn_not_all_fields), Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.name_but_save))
+        }
     }
 }
